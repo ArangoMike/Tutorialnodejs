fca2432e79ad4249751c204e02cc6b1cd67d34fc
ArangoMike <michaelarango0531@gmail.com>
2022-11-03 10:53:40 -0500

    Codigo de Tutorial nodejs sofka

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* Codigo realizado con fines academicos del punto -Tutorial Nodejs- de Sofka. */
+router.get('/cool', function(req, res, next) {
+  res.send('Eres Genial');
+});
+
+module.exports = router;
